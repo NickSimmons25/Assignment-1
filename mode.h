f7d32b77bd86a52470e74b21d66b4bc4aab37080
NickSimmons25 <48082236+NickSimmons25@users.noreply.github.com>
2019-04-28 12:17:26 +1000

    Create mode.h
    header file where the user selects the encryption/decryption function to be applied to the input text.

---
+++ mode.h
@@ -0,0 +1,13 @@
+
+/*
+  Change the value of MODE in this header to decide what the program does. 
+
+    MODE = 0  -  encryption of rotation cipher.
+    MODE = 1  -  decryption of rotation cipher with key.
+    MODE = 2  -  decryption of rotation cipher without key.
+    MODE = 3  -  encryption of substitution cipher.
+    MODE = 4  -  decryption of substitution cipher with key.
+  
+*/
+
+#define MODE 0
